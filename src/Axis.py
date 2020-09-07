ccaebd8d0f049812213c8e21080573bc01b94e4b
RyosukeMatsushima <m.ryosuke@me.com>
2020-09-07 14:51:34 +0900

    add submodule and update Axis

--- src/Axis.py
+++ src/Axis.py
@@ -5,6 +5,17 @@ class Axis:
         self.name = name
         self.min = min
         self.max =max
-        self. min_step = min_step
+        self.min_step = min_step
         self.elements = np.arange(min, max + min_step, min_step)
         self.element_count = self.elements.shape[0]
+
+    def num2val(self, num):
+        if num < 0 and self.element_count + 1 < num:
+            ArithmeticError("num is not in range")
+        return self.elements[num]
+
+    def val2num(self, val):
+        if val < self.min and self.max < val:
+            ArithmeticError("val is not in range")
+        return int(val / self.min_step)
+
--- tests/AxisTest.py
+++ tests/AxisTest.py
@@ -5,3 +5,6 @@ axis = Axis("test", 0.0, 10.2, 0.3)
 print(axis.name)
 print(axis.elements)
 print(axis.element_count)
+
+print(axis.num2val(3))
+print(axis.val2num(0.5))
