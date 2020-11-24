289be80e7d504468debc62c1f89370c442a09109
RyosukeMatsushima <m.ryosuke@me.com>
2020-11-24 18:51:22 +0900

    need reset

---
+++ tests/FuildSimulatorTest.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from src.FuildSimulator import FuildSimulator
+from src.TopologicalSpace import TopologicalSpace
+from src.Axis import Axis
+
+import numpy as np
+
+axes = (Axis("theta", -1.0, 1.0, 0.1), Axis("theta_dot", -1.0, 1.0, 0.5))
+t_s = TopologicalSpace(*axes)
+graph_center = ["theta", "theta_dot", [0, 0]]
+
+d = 1.
+u_set = np.arange(-2., 2. + d, d)
+moderate_u = 0
+inputCalculator = FuildSimulator(t_s, (0, 0), graph_center, u_set, moderate_u)
+inputCalculator.init_stochastic_matrix()
+inputCalculator.method2(0.024, 1000)
+# inputCalculator.update_astablishment_space()
+# inputCalculator.t_s.show_plot("theta", "theta_dot", [0, 0])
+
+# while(True):
+#     for i in range(1000):
+#         inputCalculator.simulate()
+#     inputCalculator.update_astablishment_space()
+#     inputCalculator.t_s.show_plot("theta", "theta_dot", [0, 0])
+#     print(inputCalculator._simulate_time)
